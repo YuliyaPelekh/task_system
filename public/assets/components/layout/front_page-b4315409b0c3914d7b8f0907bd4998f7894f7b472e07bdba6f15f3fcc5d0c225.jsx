49422ef624db4f35a543a35a837bb1d4dd63ba2c
Yuliya Pelekh <jpelekh@mail.ru>
2016-01-09 05:15:02 +0200

    New styles

---
+++ public/assets/components/layout/front_page-b4315409b0c3914d7b8f0907bd4998f7894f7b472e07bdba6f15f3fcc5d0c225.jsx
@@ -0,0 +1,18 @@
+var FrontPage = React.createClass({
+  render: function() {
+    return (
+    <div>
+      <Carousel />
+      <div className = 'main-page-container'>
+        <h1>Welcome to Task Management App!</h1>
+          <p>Task Management App is the best tool for planning your time. 
+             Hundreds of teams choose our app to capture and organize issues, 
+             assign work, and follow team activity. 
+             At your desk or on the go with the new mobile interface, 
+             the app helps your team get the job done.
+          </p>
+      </div>
+    </div>
+    );
+   }
+});
---
+++ public/assets/components/users/user_new-3d1c0db8ee8d1c04ec371240af9e70f5422d7382f0ce497533bb2188c19cc1e6.jsx
@@ -0,0 +1,28 @@
+var NewUser = React.createClass({
+  mixins: [ReactRouter.Navigation],
+
+  handleUserSubmit: function(user) {
+   $.ajax({
+      url: 'users.json',
+      dataType: 'json',
+      type: 'POST',
+      data: {user: user},
+      success: function(data) {
+        this.setState({data: data});
+        this.transitionTo("users");
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error('users.json', status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  render: function() {
+    return (
+      <div className='form-wrapper'>
+        <h1>Please register</h1>
+        <UserForm onUserSubmit={this.handleUserSubmit} />
+      </div>
+    );
+  }
+});
